1943a252a2f5e80df7b86ab443a5fa5f337cb337
cheatman-xda <cheat_m2000@yahoo.com>
2013-05-03 11:30:15 +0300

    Source commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Wed May 1 19:10:52 EEST 2013"
+#define LINUX_COMPILE_BY "cheatman"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
