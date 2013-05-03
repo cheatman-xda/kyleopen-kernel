a7bf9b7cb1fb9fc74411c18fffb133b999f49862
cheatman-xda <cheat_m2000@yahoo.com>
2013-05-03 15:41:27 +0300

    Added SIO, VR I/O schedulers and SavagedZen, InteractiveX and SmartassV2 governors

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Wed May 1 19:10:52 EEST 2013"
+#define UTS_VERSION "#2 PREEMPT Fri May 3 15:27:40 EEST 2013"
 #define LINUX_COMPILE_BY "cheatman"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8"
+#define UTS_RELEASE "3.0.8-g0259cc7-dirty"
