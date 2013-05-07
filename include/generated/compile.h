b7eb7e48b464ceb3ded6d51dc07182206025d847
cheatman-xda <cheat_m2000@yahoo.com>
2013-05-07 22:54:17 +0300

    Cleanup

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 8 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 PREEMPT Tue May 7 22:05:07 EEST 2013"
+#define UTS_VERSION "#8 PREEMPT Tue May 7 22:28:58 EEST 2013"
 #define LINUX_COMPILE_BY "cheatman"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8-ga7bf9b7-dirty"
+#define UTS_RELEASE "3.0.8-gd2e051f-dirty"
